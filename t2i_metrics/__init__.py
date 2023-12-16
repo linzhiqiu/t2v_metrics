97aa98d66a5bb167a6af3698c67f2d5078c022a2
linzhiqiu <zl279@cornell.edu>
2023-12-16 15:44:41 -0500

    [update] pip support

--- t2i_metrics/__init__.py
+++ t2i_metrics/__init__.py
@@ -3,8 +3,5 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import numpy as np
-import torch
-
 from .vqascore import VQAScore
 from .clipscore import CLIPScore
