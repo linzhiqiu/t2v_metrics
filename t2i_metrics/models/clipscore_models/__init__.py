16d3f53be13ea064c78280c67bc48f8e6ec619d4
linzhiqiu <zl279@cornell.edu>
2024-01-08 04:33:16 -0500

    [update] add pickscore v1

--- t2i_metrics/models/clipscore_models/__init__.py
+++ t2i_metrics/models/clipscore_models/__init__.py
@@ -1,12 +1,14 @@
 from .clip_model import CLIP_MODELS, CLIPScoreModel
 from .blip2_itc_model import BLIP2_ITC_MODELS, BLIP2ITCScoreModel
 from .hpsv2_model import HPSV2_MODELS, HPSV2ScoreModel
+from .pickscore_model import PICKSCORE_MODELS, PickScoreModel
 from ...constants import HF_CACHE_DIR
 
 ALL_CLIP_MODELS = [
     CLIP_MODELS,
     BLIP2_ITC_MODELS,
     HPSV2_MODELS,
+    PICKSCORE_MODELS,
 ]
 
 def list_all_clipscore_models():
@@ -20,5 +22,7 @@ def get_clipscore_model(model_name, device='cuda', cache_dir=HF_CACHE_DIR):
         return BLIP2ITCScoreModel(model_name, device=device, cache_dir=cache_dir)
     elif model_name in HPSV2_MODELS:
         return HPSV2ScoreModel(model_name, device=device, cache_dir=cache_dir)
+    elif model_name in PICKSCORE_MODELS:
+        return PickScoreModel(model_name, device=device, cache_dir=cache_dir)
     else:
         raise NotImplementedError()
