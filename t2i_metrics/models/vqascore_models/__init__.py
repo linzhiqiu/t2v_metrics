80c942aaa6b77d229b05f9b3207ffcbca499373b
Nyandwi <johnjw7084@gmail.com>
2024-01-23 23:14:52 +0200

    [add] Adding Kosmos2

--- t2i_metrics/models/vqascore_models/__init__.py
+++ t2i_metrics/models/vqascore_models/__init__.py
@@ -1,12 +1,14 @@
 from .clip_t5_model import CLIP_T5_MODELS, CLIPT5Model
 from .llava_model import LLAVA_MODELS, LLaVAModel
 from .instructblip_model import InstructBLIP_MODELS, InstructBLIPModel
+from .kosmos2_model import KOSMOS2_MODELS, Kosmos2Model
 from ...constants import HF_CACHE_DIR
 
 ALL_VQA_MODELS = [
     CLIP_T5_MODELS,
     LLAVA_MODELS,
     InstructBLIP_MODELS,
+    KOSMOS2_MODELS,
 ]
 
 def list_all_vqascore_models():
@@ -20,5 +22,7 @@ def get_vqascore_model(model_name, device='cuda', cache_dir=HF_CACHE_DIR):
         return LLaVAModel(model_name, device=device, cache_dir=cache_dir)
     elif model_name in InstructBLIP_MODELS:
         return InstructBLIPModel(model_name, device=device, cache_dir=cache_dir)
+    elif model_name in KOSMOS2_MODELS:
+        return Kosmos2Model(model_name, device=device, cache_dir=cache_dir)
     else:
         raise NotImplementedError()
