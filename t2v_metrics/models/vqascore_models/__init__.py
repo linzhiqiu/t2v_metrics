fa68b38fb18882b00c6ec70a63405ead2f3bd4eb
aldoz-mila <aldo.zaimi+1@mila.quebec>
2024-04-25 23:48:27 -0400

    add cogvlm option

--- t2v_metrics/models/vqascore_models/__init__.py
+++ t2v_metrics/models/vqascore_models/__init__.py
@@ -2,6 +2,7 @@ from .clip_t5_model import CLIP_T5_MODELS, CLIPT5Model
 from .llava_model import LLAVA_MODELS, LLaVAModel
 from .llava16_model import LLAVA16_MODELS, LLaVA16Model
 from .instructblip_model import InstructBLIP_MODELS, InstructBLIPModel
+from .cogvlm_model import COGVLM_MODELS, CogVLMModel
 from ...constants import HF_CACHE_DIR
 
 ALL_VQA_MODELS = [
@@ -9,6 +10,7 @@ ALL_VQA_MODELS = [
     LLAVA_MODELS,
     LLAVA16_MODELS,
     InstructBLIP_MODELS,
+    COGVLM_MODELS,
 ]
 
 def list_all_vqascore_models():
@@ -24,5 +26,7 @@ def get_vqascore_model(model_name, device='cuda', cache_dir=HF_CACHE_DIR):
         return LLaVA16Model(model_name, device=device, cache_dir=cache_dir)
     elif model_name in InstructBLIP_MODELS:
         return InstructBLIPModel(model_name, device=device, cache_dir=cache_dir)
+    elif model_name in COGVLM_MODELS:
+        return CogVLMModel(model_name, device=device, cache_dir=cache_dir)
     else:
         raise NotImplementedError()
